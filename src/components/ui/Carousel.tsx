7d5231148e62f3b5a352bde69b872a645b38e746
bswa006 <bswa006@gmail.com>
2025-02-05 02:18:19 +0530

    intutive slider v1

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Mousewheel } from 'swiper/modules';
 import type { Swiper as SwiperType } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
-
 import 'swiper/css/navigation';
 import styles from './Carousel.module.css';
 
@@ -13,7 +12,6 @@ interface CarouselProps {
   items: React.ReactNode[];
   slidesPerView?: number;
   spaceBetween?: number;
-  loop?: boolean;
   onSlideChange?: (swiper: SwiperType) => void;
 }
 
@@ -21,22 +19,48 @@ export function Carousel({
   items,
   slidesPerView = 3,
   spaceBetween = 12,
-  loop = false,
   onSlideChange,
 }: CarouselProps) {
+  const [isDragging, setIsDragging] = React.useState(false);
   return (
     <div className={styles['carousel-container']}>
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, Mousewheel]}
         spaceBetween={spaceBetween}
         slidesPerView={slidesPerView}
-        loop={loop}
+        speed={600}
         navigation={{
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
           disabledClass: 'swiper-button-disabled',
+          lockClass: 'swiper-button-lock'
         }}
-        onSlideChange={onSlideChange || (() => {})}
+        rewind={false}
+        mousewheel={{
+          forceToAxis: true,
+          sensitivity: 0.5,
+          thresholdDelta: 50,
+          releaseOnEdges: true,
+        }}
+        grabCursor={true}
+        slidesOffsetBefore={0}
+        slidesOffsetAfter={0}
+        resistance={false}
+        shortSwipes={false}
+        longSwipesRatio={0.3}
+        touchRatio={1.5}
+        onSlideChange={(swiper) => {
+          if (!isDragging && onSlideChange) {
+            onSlideChange(swiper);
+          }
+        }}
+        onTouchStart={() => setIsDragging(true)}
+        onTouchEnd={() => setIsDragging(false)}
+        onDragStart={() => setIsDragging(true)}
+        onDragEnd={() => {
+          setTimeout(() => setIsDragging(false), 100);
+        }}
+        onWheel={() => setIsDragging(true)}
         className={styles['swiper']}
       >
         {items.map((item, index) => (
